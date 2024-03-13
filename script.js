aff1452064fcff93c12f7f85b81b99bb3f84fd4e
kalyaz ruf <xakimov.rufat.07@gmail.com>
2024-03-13 23:13:31 +0500

    Анимация переходов это пиздец всё нахуй я уже который час пытаюсь правильно настроить ебанный js рот его ел

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const anchor = document.getElementById('wrapBut')
-
+let anchor = document.getElementById('wrapBut')
 anchor.addEventListener('click', function (event) {
     event.preventDefault();
-    const target = document.getElementById('curses'); // замени 'target' на id элемента, к которому нужно плавно прокрутить
-    target.scrollIntoView({ behavior: 'smooth' });
+    const target = document.getElementById('our-sections'); // замени 'our-sections' на id элемента, к которому нужно плавно прокрутить
+    target.scrollIntoView({ behavior: "smooth" });
 })
+
+let pythonBut = document.getElementById('py-but')
+pythonBut.addEventListener('click', function (event) {
+    event.preventDefault();
+    const target = document.getElementById('py-slide'); // замени 'our-sections' на id элемента, к которому нужно плавно прокрутить
+    target.scrollIntoView({ behavior: "smooth" });
+})
+
+
 
 
 
