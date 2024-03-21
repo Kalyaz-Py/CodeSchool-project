2379170941ac7f19eb547d2c72f72bdc22abc90f
kalyaz ruf <xakimov.rufat.07@gmail.com>
2024-03-21 22:42:25 +0500

    добавил frontend слайды

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ pythonBut.addEventListener('click', function (event) {
     target.scrollIntoView({ behavior: "smooth" });
 })
 
+let frontBut = document.getElementById('fr-but')
+frontBut.addEventListener('click', function (event) {
+    event.preventDefault();
+    const target = document.getElementById('front-slide'); // замени 'our-sections' на id элемента, к которому нужно плавно прокрутить
+    target.scrollIntoView({ behavior: "smooth" });
+})
+
 
 
 
