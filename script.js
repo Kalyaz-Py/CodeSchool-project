fa898cc7815f28b9db00a8c9a80e23a7b0e5eb4b
kalyaz ruf <xakimov.rufat.07@gmail.com>
2024-03-26 21:30:51 +0500

    Убрал костыли от JavaScript'a теперь анимации перехода без него

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-let anchor = document.getElementById('wrapBut')
-anchor.addEventListener('click', function (event) {
-    event.preventDefault();
-    const target = document.getElementById('our-sections'); // замени 'our-sections' на id элемента, к которому нужно плавно прокрутить
-    target.scrollIntoView({ behavior: "smooth" });
-})
-
-let pythonBut = document.getElementById('py-but')
-pythonBut.addEventListener('click', function (event) {
-    event.preventDefault();
-    const target = document.getElementById('py-slide'); // замени 'our-sections' на id элемента, к которому нужно плавно прокрутить
-    target.scrollIntoView({ behavior: "smooth" });
-})
-
-let frontBut = document.getElementById('fr-but')
-frontBut.addEventListener('click', function (event) {
-    event.preventDefault();
-    const target = document.getElementById('front-slide'); // замени 'our-sections' на id элемента, к которому нужно плавно прокрутить
-    target.scrollIntoView({ behavior: "smooth" });
-})
 
 
 
