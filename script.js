1fdaf5278cca68818b57c91cf426ee58822ab028
kalyaz ruf <xakimov.rufat.07@gmail.com>
2024-04-09 19:12:20 +0500

    Добавил текста, футер и остального по мелочи

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const modal = document.getElementById('modal');
 const closeButton = document.querySelector('.close');
+const sendButton = document.getElementById('button-send-form')
 
-const modal1 = document.getElementById('')
+const inputForm1 = document.getElementById('form-input1');
+const inputForm2 = document.getElementById('form-input2');
+
 // Открыть модальное окно
 function openModal() {
   modal.style.display = 'flex'; // Было 'block'
+  modal.classList.add('modal')
+}
+// Очистка формы
+function clearForm() {
+  inputForm1.value = '';
+  inputForm2.value = '';
 }
 
 // Закрыть модальное окно
 function closeModal() {
   modal.style.display = 'none';
+  clearForm();
 }
 
 // Закрытие модального окна при клике на кнопку закрытия
 closeButton.addEventListener('click', closeModal);
+
 
 // Закрытие модального окна при клике вне окна
 window.addEventListener('click', (event) => {
@@ -22,10 +33,8 @@ window.addEventListener('click', (event) => {
   }
 });
 
-// Исправленный код для центрирования модального окна
-window.addEventListener('resize', () => {
-  modal.style.display = window.innerWidth > 768 ? 'flex' : 'block';
-});
+
+
 
 
 
