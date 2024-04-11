600386d0b65954ec32059aab41c1512b75d260d2
kalyaz ruf <xakimov.rufat.07@gmail.com>
2024-04-11 22:55:56 +0500

    version 1.05 Добавлено: боковую панель Изменено: Визитки курсов Исправлены баги и проблемы

--- script.js
+++ script.js
@@ -1,85 +1,35 @@
-const modal = document.getElementById('modal');
-const closeButton = document.querySelector('.close');
-const sendButton = document.getElementById('button-send-form')
+const modal = document.getElementById("modal");
+const closeButton = document.querySelector(".close");
+const sendButton = document.getElementById("button-send-form");
 
-const inputForm1 = document.getElementById('form-input1');
-const inputForm2 = document.getElementById('form-input2');
+const inputForm1 = document.getElementById("form-input1");
+const inputForm2 = document.getElementById("form-input2");
 
 // Открыть модальное окно
 function openModal() {
-  modal.style.display = 'flex'; // Было 'block'
-  modal.classList.add('modal')
+  modal.style.display = "flex"; // Было 'block'
+  modal.classList.add("modal");
 }
 // Очистка формы
 function clearForm() {
-  inputForm1.value = '';
-  inputForm2.value = '';
+  inputForm1.value = "";
+  inputForm2.value = "";
 }
 
 // Закрыть модальное окно
 function closeModal() {
-  modal.style.display = 'none';
+  modal.style.display = "none";
   clearForm();
 }
 
 // Закрытие модального окна при клике на кнопку закрытия
-closeButton.addEventListener('click', closeModal);
-
+closeButton.addEventListener("click", closeModal);
 
 // Закрытие модального окна при клике вне окна
-window.addEventListener('click', (event) => {
+window.addEventListener("click", (event) => {
   if (event.target === modal) {
     closeModal();
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
