13c5ff0c7e1eb37219395842afc59f93049ca18d
kalyaz ruf <xakimov.rufat.07@gmail.com>
2024-05-12 23:11:31 +0500

    немного правок

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const closeButton = document.querySelector(".close");
 const sendButton = document.getElementById("button-send-form");
 
 const inputForm1 = document.getElementById("form-input1");
-const inputForm2 = document.getElementById("form-input2");
+const inputForm2 = document.getElementById("phone-input");
+const inputForm3 = document.getElementById("form-input3");
+
+
 
 // Открыть модальное окно
 function openModal() {
@@ -14,6 +17,7 @@ function openModal() {
 function clearForm() {
   inputForm1.value = "";
   inputForm2.value = "";
+  inputForm3.value = "";
 }
 
 // Закрыть модальное окно
@@ -21,6 +25,8 @@ function closeModal() {
   modal.style.display = "none";
   clearForm();
 }
+// Закрытие модального окна при клике на кнопку покупки
+sendButton.addEventListener("click", closeModal)
 
 // Закрытие модального окна при клике на кнопку закрытия
 closeButton.addEventListener("click", closeModal);
@@ -32,4 +38,24 @@ window.addEventListener("click", (event) => {
   }
 });
 
+const input = document.getElementById("phone-input");
 
+        input.addEventListener("input", function() {
+            const phoneNumber = input.value.replace(/\D/g, ""); // Удаляем все нецифровые символы
+            input.value = formatPhoneNumber(phoneNumber);
+        });
+
+        function formatPhoneNumber(phoneNumber) {
+            var formattedNumber = phoneNumber;
+            
+            if (phoneNumber.length >= 1) {
+                formattedNumber = "+7" + phoneNumber.slice(1); // Сохраняем остальные цифры номера
+            }
+            
+            // Используем регулярное выражение, чтобы добавить пробелы после каждых двух цифр
+            const matchedGroups = formattedNumber.match(/(\+\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+            return matchedGroups[1] + (matchedGroups[2] ? " " + matchedGroups[2] : "") +
+                (matchedGroups[3] ? " " + matchedGroups[3] : "") +
+                (matchedGroups[4] ? " " + matchedGroups[4] : "") +
+                (matchedGroups[5] ? " " + matchedGroups[5] : "");
+        }
